ba0cbefb30dd576b5e4b202391c635a189af50e4
Bastien181 <bastien.pil01@gmail.com>
2024-11-10 16:29:32 +0100

    Finded us ticker location need to find a low power replacement solution

---
+++ deploy_dev_env.sh
@@ -0,0 +1,235 @@
+
+BLUE='\033[0;34m'
+WHITE='\033[0;37m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+# Helper functions
+
+# $1=file name, $2=file sha256, $3=file url, $4=POST arguments (optional)
+download_archive() {
+    if [[ $(sha256sum $1) = "$2  $1" ]]; then
+        echo "$1 already downloaded."
+    else
+        echo "File not valid or inexistant. Downloading..."
+        if ! [ -z "$4" ]; then
+            wget -O $1 --post-data=$4 $3 --no-check-certificate
+        else
+            wget -O $1 $3 --no-check-certificate
+        fi
+        if ! [[ $(sha256sum $1) = "$2  $1" ]]; then
+            echo -e "${RED}$1 download failure${WHITE}"
+            exit
+        fi
+    fi
+}
+
+# Prerequisites
+echo -e "${BLUE}Installing prerequisites${WHITE}"
+
+sudo apt update
+sudo apt install wget -y
+sudo apt install make -y
+sudo apt install gcc -y
+
+ENVDIR=/home/$(whoami)/mbed-dev-env
+mkdir -p $ENVDIR
+cd $ENVDIR
+
+mkdir -p $ENVDIR/.tmp
+
+# VSCode
+echo -e "${BLUE}Installing VSCode${WHITE}"
+cd $ENVDIR/.tmp
+
+download_archive "vscode_linux_x64_stable.tar.gz" "ef47904485951cf557a7402a40b163291c3131d420efe0d9e443dbd513b7ca82" "https://az764295.vo.msecnd.net/stable/4cb974a7aed77a74c7813bdccd99ee0d04901215/code-stable-x64-1686587897.tar.gz"
+
+echo "Installing VSCode to environment"
+tar -xvzf vscode_linux_x64_stable.tar.gz
+cp -r *VSCode* $ENVDIR/vscode
+cd $ENVDIR/vscode
+mkdir data
+cd bin
+./code --install-extension ms-vscode.cpptools
+./code --install-extension ms-vscode.cpptools-extension-pack
+./code --install-extension ms-vscode.cpptools-themes
+./code --install-extension nemesv.copy-file-name
+./code --install-extension marus25.cortex-debug@1.4.4
+./code --install-extension SanaAjani.taskrunnercode
+./code --install-extension tomoki1207.pdf
+
+## Compiler
+echo -e "${BLUE}Installing ARM GCC toolchain${WHITE}"
+cd $ENVDIR/.tmp
+
+download_archive "gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2" "5adc2ee03904571c2de79d5cfc0f7fe2a5c5f54f44da5b645c17ee57b217f11f" "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2"
+
+tar -xvf gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
+cp -r gcc-arm-none-eabi-9-2020-q2-update $ENVDIR/gcc-arm-none-eabi
+touch $ENVDIR/gcc-arm-none-eabi/gcc_version_9-2020-q2
+
+
+## Python
+echo -e "${BLUE}Installing Python3${WHITE}"
+cd $ENVDIR/.tmp
+
+sudo apt install -y libffi-dev libgdbm-dev libsqlite3-dev libssl-dev zlib1g-dev
+
+download_archive "openssl-1.1.1g.tar.gz" "ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46" "https://www.openssl.org/source/openssl-1.1.1g.tar.gz"
+
+if ! test -d "$ENVDIR/openssl"; then
+    tar -zxvf openssl-1.1.1g.tar.gz
+    cd openssl-1.1.1g
+
+    ./config --prefix=$ENVDIR/openssl --openssldir=$ENVDIR/openssl/ssl no-ssl2
+    make
+    #make test
+    make install_sw
+fi
+
+export LD_LIBRARY_PATH=$ENVDIR/openssl/lib:$LD_LIBRARY_PATH
+
+cd $ENVDIR/.tmp
+download_archive "Python-3.7.17.tgz" "fd50161bc2a04f4c22a0971ff0f3856d98b4bf294f89740a9f06b520aae63b49" "https://www.python.org/ftp/python/3.7.17/Python-3.7.17.tgz"
+
+tar -xvzf Python-3.7.17.tgz
+cd Python-3.7.17
+
+#LDFLAGS="-L$ENVDIR/openssl/lib"
+
+./configure \
+    --prefix=$ENVDIR/python-install/python-3.7.17/ \
+    --enable-shared \
+    --enable-ipv6 \
+    --with-openssl=$ENVDIR/openssl \
+    LDFLAGS=-Wl,-rpath=$ENVDIR/python-install/python-3.7.17/lib,--disable-new-dtags,-L$ENVDIR/openssl/lib
+
+make -j8
+
+OLDPATH=$PATH
+PATH=$PATH:$ENVDIR/python-install/python-3.7.17/bin/
+make install
+
+# Copy the openssl libraries to the python lib folder
+cp -r $ENVDIR/openssl/lib/* $ENVDIR/python-install/python-3.7.17/lib/
+
+cd $ENVDIR/python-install/python-3.7.17/bin/
+
+wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py --no-check-certificate
+sudo ./python3 get-pip.py
+
+./python3 -m pip install wheel
+./python3 -m pip install python-dateutil
+./python3 -m pip install pyparsing
+
+# Install mbed cli requirements
+wget -O mbed-requirements.txt https://raw.githubusercontent.com/ARMmbed/mbed-os/master/requirements.txt --no-check-certificate
+./python3 -m pip install -r mbed-requirements.txt
+rm -f mbed-requirements.txt
+sudo apt install mercurial
+./python3 -m pip install mbed-cli --target $ENVDIR/mbed-cli
+./python3 -m pip install mbed-cli
+
+PATH=$OLDPATH
+
+# DIRENV
+echo -e "${BLUE}Installing direnv${WHITE}"
+sudo apt-get install direnv -y
+if ! cat ~/.bashrc | grep -a 'eval "$(direnv hook bash)"'; then
+    echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+fi
+
+# Openocd
+echo -e "${BLUE}Installing openocd${WHITE}"
+#sudo apt install openocd=0.11.0-1
+cd $ENVDIR/.tmp
+git clone https://github.com/openocd-org/openocd openocd
+cd openocd
+git checkout 9ea7f3d
+sudo apt install libtool -y
+sudo apt install pkg-config -y
+sudo apt install libusb-1.0-0-dev -y
+sudo apt install autoconf -y
+sudo apt install automake -y
+sudo apt install texinfo -y
+./bootstrap
+./configure --prefix=$ENVDIR/openocd --exec-prefix=$ENVDIR/openocd/bin --enable-jlink --enable-stlink
+make
+mkdir -p $ENVDIR/openocd
+make install
+
+# Jlink
+echo -e "${BLUE}Installing JLink${WHITE}"
+cd $ENVDIR/.tmp
+download_archive "JLink_Linux_V788h_x86_64.tgz" "cde202c17376b4b77a99d66794c819dcf312695be783a951aaf99cd69e449612" "https://www.segger.com/downloads/jlink/JLink_Linux_V788h_x86_64.tgz" "accept_license_agreement=accepted"
+tar -xvzf JLink_Linux_V788h_x86_64.tgz
+cd JLink_Linux_V788h_x86_64
+mkdir -p $ENVDIR/jlink
+cp -r . $ENVDIR/jlink
+
+# Fixup
+## USB permission fixup
+sudo usermod -a -G dialout $USER
+sudo usermod -a -G plugdev $USER
+sudo wget -O /etc/udev/rules.d/49-stlinkv3.rules https://raw.githubusercontent.com/stlink-org/stlink/master/config/udev/rules.d/49-stlinkv3.rules --no-check-certificate
+sudo wget -O /etc/udev/rules.d/49-stlinkv2.rules https://raw.githubusercontent.com/stlink-org/stlink/master/config/udev/rules.d/49-stlinkv2.rules --no-check-certificate
+sudo wget -O /etc/udev/rules.d/49-stlinkv1.rules https://raw.githubusercontent.com/stlink-org/stlink/master/config/udev/rules.d/49-stlinkv1.rules --no-check-certificate
+sudo wget -O /etc/udev/rules.d/49-stlinkv2-1.rules https://raw.githubusercontent.com/stlink-org/stlink/master/config/udev/rules.d/49-stlinkv2-1.rules --no-check-certificate
+sudo udevadm control --reload
+
+# Cleanup
+echo "Cleaning temp folder"
+rm -rf $ENVDIR/.tmp
+
+# Creating utilities
+echo -e "${BLUE}Creating utilities${WHITE}"
+
+mkdir -p $ENVDIR/utilities
+
+touch $ENVDIR/utilities/envrc.content
+echo "MBEDENVDIR=/home/$(whoami)/mbed-dev-env" > $ENVDIR/utilities/envrc.content
+#echo "PATH_add \$MBEDENVDIR/python-install/python-3.7.17/bin/" >> $ENVDIR/utilities/envrc.content
+echo "PATH_add \$MBEDENVDIR/mbed-cli/bin/" >> $ENVDIR/utilities/envrc.content
+echo "PATH_add \$MBEDENVDIR/vscode/bin/" >> $ENVDIR/utilities/envrc.content
+echo "PATH_add \$MBEDENVDIR/gcc-arm-none-eabi/bin/" >> $ENVDIR/utilities/envrc.content
+echo "PATH_add \$MBEDENVDIR/openocd/bin/bin" >> $ENVDIR/utilities/envrc.content
+echo "PATH_add \$MBEDENVDIR/jlink" >> $ENVDIR/utilities/envrc.content
+echo "echo 'Mbed environment with VSCode has been setup and is ready to be used.'"  >> $ENVDIR/utilities/envrc.content
+
+touch $ENVDIR/utilities/mbedify.sh
+chmod +x $ENVDIR/utilities/mbedify.sh
+echo "cp -f /home/$(whoami)/mbed-dev-env/utilities/envrc.content ./.envrc" > $ENVDIR/utilities/mbedify.sh
+echo "direnv allow" >> $ENVDIR/utilities/mbedify.sh
+
+if ! grep -q "export PATH=\$PATH:$ENVDIR/utilities/" "/home/$(whoami)/.bashrc"; then
+  echo "export PATH=\$PATH:$ENVDIR/utilities/" >> /home/$(whoami)/.bashrc
+fi
+
+
+echo -e "${GREEN}Successfully installed Mbed OS development environment${WHITE}"
+echo "Installation folder: /home/$(whoami)/mbed-dev-env"
+echo -e "Summary of the installed components:"
+echo -e "  - VScode with extensions (command(s): ${BLUE}code${WHITE})"
+echo -e "  - Mbed CLI 1 (command(s): ${BLUE}mbed${WHITE})"
+echo -e "  - ARM-GCC toolchain 9 (command(s): ${BLUE}arm-none-eabi-xxx${WHITE})"
+echo -e "  - OpenOCD 0.12.0 (command(s): ${BLUE}openocd${WHITE})"
+echo -e "  - JLink commander 7.88h (command(s): ${BLUE}JLinkExe${WHITE})"
+echo "==============================="
+echo -e "${BLUE}Usage instruction${WHITE}"
+echo "To use this environment automatically, create a '.envrc' file at the root of directory in which you want to have access to the environment"
+echo "Content of '.envrc' file:"
+echo "==============================="
+echo "MBEDENVDIR=/home/$(whoami)/mbed-dev-env"
+echo "PATH_add \$MBEDENVDIR/mbed-cli/bin/"
+echo "PATH_add \$MBEDENVDIR/vscode/bin/"
+echo "PATH_add \$MBEDENVDIR/gcc-arm-none-eabi/bin/"
+echo "PATH_add \$MBEDENVDIR/openocd/bin/bin"
+echo "PATH_add \$MBEDENVDIR/jlink"
+echo "==============================="
+echo "Finally, run 'direnv allow' within the folder to enable the automatic environment setup when you enter the folder."
+echo ""
+echo ""
+echo -e "IMPORTANT: You can also use '${BLUE}mbedify${WHITE}' command in the folder you want to automatically deploy the above content."
+echo ""
+echo ""
+echo "Installation done"
--- main.cpp
+++ main.cpp
@@ -1,31 +1,47 @@
-/**
- * @file main.cpp
- * @author Yann Charbon <yann.charbon@heig-vd.ch>, <yann.charbon@ik.me>
- * @brief System entry point.
- * @version 1.0
- * @date 2022-10-11
+/*
+ * Copyright (c) 2016 ARM Limited. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @copyright Copyright (c) 2022
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 #include "mbed.h"
 #include "Thread.h"
 #include "NanostackInterface.h"
 #include "EthernetInterface.h"
 #include "mbed-trace/mbed_trace.h"
+#include "mesh_nvm.h"
 
 #include "PinNames.h"
 
 #define TRACE_GROUP "APP"
 
 #ifdef EFR32_BETECH
-DigitalOut oscillator39MhzEnable(OSC_CC_EN);
+// DigitalOut oscillator39MhzEnable(OSC_CC_EN);
 #endif
 
-
-void trace_printer(const char* str) {
+void trace_printer(const char *str)
+{
     printf("%s\n", str);
+}
+
+void thread_eui64_trace()
+{
+#define LOWPAN 1
+#define THREAD 2
+#if MBED_CONF_NSAPI_DEFAULT_MESH_TYPE == THREAD && (MBED_VERSION >= MBED_ENCODE_VERSION(5,10,0))
+   uint8_t eui64[8] = {0};
+   static_cast<ThreadInterface*>(mesh)->device_eui64_get(eui64);
+   printf("Device EUI64 address = %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x\n", eui64[0], eui64[1], eui64[2], eui64[3], eui64[4], eui64[5], eui64[6], eui64[7]);
+#endif
 }
 
 static Mutex SerialOutMutex;
@@ -40,12 +56,12 @@ void serial_out_mutex_release()
     SerialOutMutex.unlock();
 }
 
-MeshInterface *mesh ;
+MeshInterface *mesh;
 
 #ifdef EFR32_CUSTOM_BOARD
 DigitalOut enableVCOM(ENABLE_VCOM);
+DigitalOut led1(LED1);
 #endif
-
 
 int main()
 {
@@ -54,24 +70,82 @@ int main()
 #endif
 
 #ifdef EFR32_BETECH
-    oscillator39MhzEnable = 0;
+    // oscillator39MhzEnable = 0;
 #endif
+    /*int i = 0;
+    while (1)
+    {
+        ThisThread::sleep_for(500ms);
+        printf("Loop %d\n", i++);
+        ThisThread::sleep_for(1s);
+        printf("Loop %d\n", i++);
+        ThisThread::sleep_for(2s);
+        printf("Loop %d\n", i++);
+    }*/
+
+    /*bool can_deep = sleep_manager_can_deep_sleep();
+    printf("Deep sleep allowed: %i\r\n", can_deep);
+
+    while(1){
+        printf("Deep sleep allowed: %i\r\n", sleep_manager_can_deep_sleep());
+        ThisThread::sleep_for(2s);
+        led1 = !led1;
+    }*/
+
+    /*bool can_deep = sleep_manager_can_deep_sleep();
+    printf("Deep sleep allowed: %i\r\n", can_deep);
+
+    bool toggle = true;
+    led1 = 1;
+
+    while (1) {
+        if (toggle) {
+            printf("Lock deep sleep\n");
+            sleep_manager_lock_deep_sleep();
+        } else {
+            printf("Unlock deep sleep\n");
+            sleep_manager_unlock_deep_sleep();
+        }
+        printf("Deep sleep allowed: %i\n\n", sleep_manager_can_deep_sleep());
+        toggle = !toggle;
+        //ThisThread::sleep_for(2s);
+        for (int i = 0; i < 20000000; i++) {
+            asm("nop");
+        }
+        led1 = !led1;
+    }*/
+
     bool can_deep = sleep_manager_can_deep_sleep();
     printf("Deep sleep allowed: %i\r\n", can_deep);
 
     mbed_trace_init();
     mbed_trace_print_function_set(trace_printer);
-    mbed_trace_mutex_wait_function_set( serial_out_mutex_wait );
-    mbed_trace_mutex_release_function_set( serial_out_mutex_release );
+    mbed_trace_mutex_wait_function_set(serial_out_mutex_wait);
+    mbed_trace_mutex_release_function_set(serial_out_mutex_release);
 
-    /* *** Head over to user application *** */
+    printf("Start mesh-minimal application\n");
+
+#define STR(s) #s
+    printf("Build: %s %s\nMesh type: %s\n", __DATE__, __TIME__, STR(MBED_CONF_NSAPI_DEFAULT_MESH_TYPE));
+#ifdef MBED_MAJOR_VERSION
+    printf("Mbed OS version: %d.%d.%d\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+#endif
+
     mesh = MeshInterface::get_default_instance();
-    if (!mesh) {
+    if (!mesh)
+    {
         printf("Error! MeshInterface not found!\n");
         return -1;
     }
+    
+    while(1){
+        printf("Deep sleep allowed: %i\r\n", sleep_manager_can_deep_sleep());
+        ThisThread::sleep_for(2s);
+    }
 
-    printf("Connecting to the mesh...\n");
+    thread_eui64_trace();
+    mesh_nvm_initialize();
+    printf("Connecting...\n");
     int error = mesh->connect();
     if (error) {
         printf("Connection failed! %d\n", error);
@@ -80,13 +154,9 @@ int main()
 
     SocketAddress sockAddr;
     while (NSAPI_ERROR_OK != mesh->get_ip_address(&sockAddr))
-        ThisThread::sleep_for(500ms);
+        ThisThread::sleep_for(500);
 
     printf("Connected. IP = %s\n", sockAddr.get_ip_address());
 
-    //mts_framework_send_user_event(MTS_WISUN_STATE_CONNECTED, (void*)sockAddr.get_ip_address(), strlen(sockAddr.get_ip_address()) + 1);
 
-    while (1) {
-        ThisThread::sleep_for(40s);
-    }
 }
